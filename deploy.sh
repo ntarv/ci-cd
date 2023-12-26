f45cc8e28c2c386fa35430e347ebf1ec6a0885b9
Taras <141884340+ntarv@users.noreply.github.com>
2023-12-26 03:04:14 +0300

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
- kubectl create namespace web1
- kubectl create deployment web-test -n web --image tardocker/web-test:latest
- kubectl scale deployment web-test -n web --replicas 3
- kubectl expose deployment web-test -n web --type=LoadBalancer --port 8000  --external-ip=92.53.116.187
